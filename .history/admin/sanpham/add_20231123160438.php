61c7f2497807ce6d28eb9b63793d5b595d7c2b53
WuocThun <binngoc1963@gmail.com>
2023-11-23 18:30:47 +0700

    update

---
+++ .history/admin/sanpham/add_20231123160438.php
@@ -0,0 +1,73 @@
+<div class="row container_dm">
+<style>
+        .container_dm {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+        .text_dm {
+            width: 100%;
+            text-align: center;
+        }
+        .btn_container_dm {
+            display: flex;
+            justify-content: center;
+            gap: 10px;
+        }
+        .formcontent {
+                        width: 50% !important;
+                    }
+                    .form_dm {
+                        width: 100%;
+                        margin: auto;
+                    }
+    </style>
+            <div class="row frmtitle">
+                <H1>Thêm mới loại hàng hoá</H1>
+            </div>
+    
+            <div class="row formcontent">
+                <form class="form_dm" action="index.php?act=addsp" method="post" class="box_dm" enctype="multipart/form-data">
+                <select name="iddm" id="">
+                        <?php 
+                        foreach ($listdanhmuc as $danhmuc){
+                            extract($danhmuc);
+                            // echo '<option vlaue="'.$id.'">"'.$namedm.'" </option>';
+                            echo '<option value="' . $id . '">' . $namedm . '</option>';
+                        }
+                    ?>
+                  </select>
+                    <div class="row mb10 text_dm">
+                    Tên món ăn<br>
+                    <input type="text" name="namesp">
+                    </div>
+                    <div class="row mb10 text_dm">
+                        Giá món ăn<br>
+                        <input placeholder="ex: 190.000" type="number" name="pricesp">
+                    </div>
+                    <div class="row mb10 text_dm">
+                        Giá món đã sale<br>
+                        <input placeholder="ex: 190.000" type="number" name="newpricesp">
+                    </div>
+                    <div class="row mb10 text_dm">
+                        Hình ảnh sản phẩm<br>
+                        <input type="file" name="imgsp">
+                    </div>
+                    <div class="row mb10 text_dm">
+                        Mô tả món ăn <br>
+                        <textarea name="des" id="" cols="83%" rows="10"></textarea>
+                    </div>
+                    <div class="row mb20 btn_container_dm">
+                        <input type="submit" class="btn_dm" name="themmoi" value="Thêm">
+                        <input type="reset" class="btn_dm" value="Nhập lại">
+                        <a class="btn_dm" href="index.php?act=listsp"><input class="btn_dm"  type="button" value="Danh Sách"></a>
+                    </div>
+                    <?php
+                    if(isset($thongbao) && ($thongbao != ""))
+                        echo '<div class="thongbao" >'.$thongbao.'</div>';
+                    ?>
+                </form>
+            </div>
+        </div>
+    </div>
---
+++ .history/admin/sanpham/update_20231123160918.php
@@ -0,0 +1,81 @@
+<?php
+    if(is_array($sanpham)) {
+        extract($sanpham);
+    }
+    $hinhpath = "../upload/".$imgsp;
+        if(is_file($hinhpath)) {
+            $hinh ="<img src='".$hinhpath."' height='80'>";
+        }else {
+            $hinh = "No photo";
+        }
+?>
+    <div class="row ">
+            <div class="row frmtitle">
+                <H1>Cập nhập loại hàng hoá</H1>
+            </div>
+            <div class="row formcontent">
+                <style>
+                    .form_ud{ 
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        flex-direction: column;
+                        width: 50%;
+                        margin: auto;
+                        text-align: center;
+                    }
+
+                    .form_ud .row {
+                        font-weight: 500;
+                        font-size: 1.3rem;
+                        text-transform: uppercase;
+                        margin: 10px;
+                    }
+                </style>
+                    <form class="form_ud" action="index.php?act=updatesp" method="post" enctype="multipart/form-data">
+                        <div class="row mb10">
+                            <select name="iddm">
+                                <option value="0" selected>Tất cả</option>
+                                <?php
+                                    foreach ($listdanhmuc as $danhmuc) {
+                                        if ($iddm == $id) echo '<option value="' . $danhmuc['id'] . '" selected>' . $danhmuc['namedm'] . '</option>';
+
+                                        else echo '<option value="' . $danhmuc['id'] . '" >' . $danhmuc['namedm'] . '</option>';   
+                                    }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="row mb10">
+                            Tên sản phẩm<br>
+                            <input type="text" name="namesp" value="<?=$namesp?>">
+                        </div>
+                        <div class="row mb10">
+                            Giá mới<br>
+                            <input type="text" name="pricesp" value="<?=$pricesp?>">
+                        </div>
+                        <div class="row mb10">
+                            Giá cũ<br>
+                            <input type="text" name="pricesp" value="<?=$newpricesp?>">
+                        </div>
+                        <div class="row mb10">
+                            Hình sản phẩm<br>
+                            <input type="file" name="imgsp">
+                            <?=$hinh?>
+                        </div>
+                        <div class="row mb10">
+                            Mô tả sản phẩm<br>
+                            <textarea style="padding:10px" name="des" cols="30" rows="10" ><?=$des?></textarea>
+                        </div>
+                        <div class="row mb20 row_form_btn">
+                            <input type="hidden" name="id" value="<?=$id?>">
+                            <input type="submit" name="capnhat" value="Cập Nhật">
+                            <input type="submit" value="Nhập lại">
+                            <a href="index.php?act=listsp"><input type="button" value="Danh Sách"></a>
+                        </div>
+                        <?php 
+                        if(isset($thongbao) && ($thongbao != "")) echo $thongbao;
+                        ?>
+                    </form>
+            </div>
+        </div>
+    </div>
