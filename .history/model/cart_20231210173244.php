0eb43985b668780d988fe4b4ca5452737bc552b6
WuocThun <binngoc1963@gmail.com>
2023-12-10 17:51:37 +0700

    QT-10/12

---
+++ .history/model/cart_20231210173244.php
@@ -0,0 +1,88 @@
+<?php
+function view_cart(){
+    global  $img_path;
+    $tong = 0;
+    $i= 0;
+
+        echo '      
+
+        <tr>
+        <td>Ảnh món ăn</td>
+        <td>Tên món ăn<br>
+        <td>Giá<br>
+        <td>Số lượng<br>
+        <td>Thành tiền<br>
+        </td>
+        <td></td>
+    </tr>';
+
+    foreach ($_SESSION['mycart'] as $cart){
+        $hinhsp = $img_path.$cart[2];
+        $ttien = $cart[3] * $cart[4];
+        $tong += $ttien;
+        $xoasp= '<a href="index.php?act=delcart&idcart='.$i.'"><input type="button" value="Xoá"></a>';
+       
+        echo '
+        
+        <tr>
+        <td><img src="'.$hinhsp.'" height="80px" alt=""></td>
+        <td>'.$cart[1].'<br>
+        <td>'.$cart[3].',000<br>
+        <td>'.$cart[4].'<br>
+        <td>'.$ttien.',000<br>
+        </td>
+         <td>'.$xoasp.'</td>
+          <!--  <td><button>Xóa</button></td> -->
+    </tr>
+
+    ';
+    $i+=1;
+
+}
+
+echo'
+<span>Tổng tiền:'.$tong.',000</span>  <a href="index.php?act=thanhtoan"><button>Thanh toán</button></a>
+';
+}
+function view_cart_noPay() {
+    global  $img_path;
+    $tong = 0;
+    $i= 0;
+
+        echo '  
+        <table>
+    
+        <tr>
+        <td>Ảnh món ăn</td>
+        <td>Tên món ăn<br>
+        <td>Giá<br>
+        <td>Số lượng<br>
+        <td>Thành tiền<br>
+        </td>
+        <td></td>
+    </tr>
+
+    ';
+
+    foreach ($_SESSION['mycart'] as $cart){
+        $hinhsp = $img_path.$cart[2];
+        $ttien = $cart[3] * $cart[4];
+        $tong += $ttien;
+        $xoasp= '<a href="index.php?act=delcart&idcart='.$i.'"><input type="button" value="Xoá"></a>';
+        echo '
+        <tr>
+        <td><img src="'.$hinhsp.'" height="80px" alt=""></td>
+        <td>'.$cart[1].'<br>
+        <td>'.$cart[3].',000<br>
+        <td>'.$cart[4].'<br>
+        <td>'.$ttien.',000<br>
+        </td>
+         <td>'.$xoasp.'</td>
+          <!--  <td><button>Xóa</button></td> -->
+    </tr>
+    ';
+    $i+=1;
+
+}
+echo'<span>Tổng tiền:'.$tong.',000</span>';    
+}
---
+++ .history/view/thanhtoan_20231210165934.php
@@ -0,0 +1,314 @@
+<style>
+.spct {
+                color: white;
+                display: flex;
+            }
+
+            .tsp {
+                width: 50%;
+                flex-direction: column;
+            }
+
+            #tsp {
+                width: 50%;
+
+            }
+
+        .tsp img {
+            width: 70%;
+            margin-left: 20%;
+        }
+        .tsp h1{
+            padding-bottom: 5%;
+            font-size: 40px;
+            color: rgb(255, 254, 254);
+            text-shadow: 2px 2px 2px #c92727;
+        }
+        .tsp strong {
+            color: rgb(245, 113, 113);
+        }
+
+            .tsp p {
+                line-height: 1.6em;
+                width: 90%;
+                height: 35%;
+                max-height: 35%;
+            }
+
+            .tsp button,
+            .tsp a button,#_button{
+                margin-top: 10%;
+                border: none;
+                background-color: brown;
+                color: white;
+                box-shadow: 0px 0px 5px brown;
+                opacity: 1;
+                /* Giảm độ mờ khi hover */
+                width: 200px;
+                padding: 15px;
+                font-size: 15px;
+               
+            }
+            
+
+            #_button{
+                margin-left: 10%;
+                margin-top: 37%;
+            }
+
+
+
+            .tsp button:hover,
+            .tableBl button:hover,#_button:hover {
+                opacity: 0.7;
+                /* Giảm độ mờ khi hover */
+
+
+            }
+
+            .tableBl button{
+                border: none;
+                background-color: brown;
+                color: white;
+                padding: 1%;
+                width: 15%;
+                box-shadow: 0px 0px 5px brown;
+                opacity: 1;
+                /* Giảm độ mờ khi hover */
+                transition: opacity 0.3s ease;
+
+            }
+
+            .tableBl {
+                margin-top: 10%;
+                margin-left: 10%
+            }
+
+            .tableBl table {
+                width: 100%;
+
+            }
+
+            .bl {
+                flex-direction: row;
+                border-collapse: collapse;
+                color: white;
+                text-align: center;
+                margin-top: 5%;
+                margin-bottom: 5%;
+                margin-left: 5%;
+            }
+
+            .bl th,
+            td {
+                width: 10%;
+                padding: 2%;
+                text-align: center;
+
+            }
+
+            .bl th {
+                margin-left: 10%;
+                font-size: 20px;
+                background-color: brown;
+                box-shadow: 0px 0px 5px brown;
+
+            }
+
+            .tableBl textarea {
+                padding: 2%;
+                width: 90%;
+                background-color: black;
+                box-shadow: 2px 2px 5px #888888;
+                color: white;
+
+            }
+
+            .row-flex {
+                display: flex;
+                flex-direction: row;
+            }
+            .row-flex a{
+                margin-left: 3%;
+            }
+
+            .tableBl textarea:hover {
+                background-color: white;
+                color: black;
+            }
+
+            .rating {
+                display: inline-block;
+            }
+
+            .rating input {
+                display: none;
+            }
+
+            .rating label {
+                float: right;
+                cursor: pointer;
+                color: #ccc;
+                transition: color 0.3s;
+            }
+
+            .rating label:before {
+                content: '\2605';
+                font-size: 20px;
+            }
+
+            .rating input:checked~label,
+            .rating label:hover,
+            .rating label:hover~label {
+                color: brown;
+                transition: color 0.3s;
+            }
+            .binhl{
+                margin-left:-10%;
+            }
+ </style>
+<style>
+.thanhtoan video {
+    width: 100%;
+    margin-top: -6%;
+}
+
+.tongtt {
+    background-color: rgba(11, 11, 11, 0.3);
+    /* Màu nền cho thẻ nav */
+    transform: translate(60%, -121%);
+    /* Để căn giữa thẻ nav chính xác */
+    color: white;
+    width: 37%;
+    margin-left: 30%;
+    text-align: center;
+    padding: 3%;
+    border-radius: 100px;
+    box-shadow: 0px 0px 5px black;
+    margin-bottom: -40%;
+}
+
+.tongtt h1 {
+    margin-bottom: 10%;
+    font-size: 33px;
+    color: rgb(255, 254, 254);
+    text-shadow: 2px 2px 2px #c92727;
+}
+
+#hoten {
+    width: 47%;
+    margin-right: 2%;
+}
+
+
+.tongtt input {
+    border-top: none;
+    border-left: none;
+    border-right: none;
+    background: none;
+    color: white;
+    padding: 1%;
+    margin-bottom: 5%;
+}
+
+.tongtt input::placeholder {
+    color: white;
+    font-weight: bold;
+}
+
+.tongtt input[type="text"] {
+    width: 100%;
+}
+
+.tongtt input[type="submit"] {
+    width: 35%;
+    padding: 2.5%;
+    margin-top: 5%;
+    font-size: 15px;
+    box-shadow: 0px 0px 2px brown;
+    background-color: brown;
+    border-radius: 50px;
+    color: white;
+}
+
+.tongtt button:hover {
+    opacity: 0.7;
+    /* Giảm độ mờ khi hover */
+
+
+}
+
+
+.error {
+    text-align: left;
+    color: brown;
+    display: none;
+    margin-top: -20px;
+}
+</style>
+
+</head>
+<div class="thanhtoan">
+    <video loop autoplay src="img/ssvd.mp4"></video>
+    <?php
+if(isset($_SESSION['userdn'])){
+    $name= $_SESSION['userdn']['user'];
+    $nameuser= $_SESSION['userdn']['nameuser'];
+    $addr= $_SESSION['userdn']['addr'];
+    $tel= $_SESSION['userdn']['tel'];
+    $name= $_SESSION['userdn']['user'];
+    $id= $_SESSION['userdn']['id'];
+}else{
+    $name= "";
+    $addr= "";
+    $tel= "";
+    $name= "";
+};
+
+?>
+<style>
+.mb-row1{
+width: 30%;
+float: left;
+}
+.mb-row2{
+width: 20%;
+float: right;
+}
+.flex-row-inter{
+/* width: 80%; */
+}
+</style>
+
+<div class="flex-row-inter">
+    <div  class="tongtt mb-row1">
+        <h1>THÔNG TIN KHÁCH HÀNG</h1>
+        <form onsubmit="return thanhtoan()" method="post">
+            <input type="text" name="nameuser" placeholder="Tn" value="<?=$nameuser?>" id="namett">
+            <br>
+            <div id="namettError" class="error"></div>
+            <br><input type="text" name="user" placeholder="Tn" value="<?=$name?>" id="namett"> <br>
+            <div id="accError" class="error"></div>
+            <br><input type="text" value="<?=$tel?>" maxlength="10" id="teltt"> <br>
+            <div id="telError" class="error"></div>
+            <br><input type="text" value="<?=$addr?>" id="addtt"> <br>
+            <div id="addError" class="error"></div><br>
+            <input type="checkbox">Thanh toán sau khi nhận hàng <br>
+            <input type="submit" value="Thanh Toán" `>
+            <h4>Bạn cần nhập thêm thông tin để hoàn thiện
+            <input type="hidden" name="id" value="$id">
+        </form>
+
+    </div>
+
+<div  class="tongtt mb-row2">
+<table>
+    <?php
+        view_cart_noPay();
+        ?>
+        </table>
+</div>    
+
+    </div>
+    <script src="/FPT-duan1/view/style/scrip.js"></script>
+</div>
