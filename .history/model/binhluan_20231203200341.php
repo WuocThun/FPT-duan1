019a7807ac83fed1ad00c78117bffa3abb34d9fb
WuocThun <binngoc1963@gmail.com>
2023-12-13 22:38:43 +0700

    add

--- .history/model/binhluan_20231203200341.php
+++
@@ -1,9 +0,0 @@
-<?php
-function doadAll_bl()
-{
-    $sql = "select * from danhmuc";
-    $listbl = pdo_query($sql);
-    return $listbl;
-}
-function insert_Bl($comment, $)
-?>
--- .history/model/taikhoan_20231127172154.php
+++
@@ -1,6 +0,0 @@
-<?php
-function insert_taikhoan($user,$pass,$email,$addr,$tel) {
-    $sql = "insert into taikhoan(user,pass,email,addr,tel) values ('$user','$pass','$email','$addr','$tel')"
-    pdo_execute($sql);
-    
-}
